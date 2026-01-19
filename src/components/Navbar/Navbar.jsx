9face262ea29ba2b77453735945e05ead83d68ee
Biswarup Pal <biswarup743428@gmail.com>
2026-01-19 19:10:13 +0530

    Fix case-sensitive import issues

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
+import { useState } from "react";
+import './Navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="Delight Swagata logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#menu">Menu</a></li>
+        <li className="p__opensans"><a href="#awards">Awards</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
+      
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">Log In </a>
+        <div />
+        <a href="/" className="p__opensans">Book a Table</a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <ul className="app__navbar-smallscreen_links">
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>Home</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>About</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li><a href="#awards" onClick={() => setToggleMenu(false)}>Awards</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+       <h1 className="logo">Indian Kitchen</h1>
+
+      <div className={`nav-links ${open ? "open" : ""}`}>
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+      
+    </nav>
+
+    
+  );
+};
+
+export default Navbar;
---
+++ src/constants/data.js
@@ -0,0 +1,79 @@
+// src/constants/data.js
+import images from './images';
+
+const wines = [
+  {
+    title: 'Paneer Butter Masala',
+    price: '₹280',
+    tags: 'Paneer | Tomato gravy | Cream | North Indian',
+  },
+  {
+    title: 'Chole Bhature',
+    price: '₹150',
+    tags: 'Chickpea curry | Deep fried bhature | Punjabi',
+  },
+  {
+    title: 'Masala Dosa',
+    price: '₹120',
+    tags: 'Rice crepe | Potato masala | South Indian',
+  },
+  {
+    title: 'Samosa (2 pcs)',
+    price: '₹80',
+    tags: 'Potato | Peas | Crispy pastry | Street snack',
+  },
+  {
+    title: 'Paneer Tikka',
+    price: '₹220',
+    tags: 'Marinated paneer | Tandoori | Appetizer',
+  },
+];
+
+const cocktails = [
+  {
+    title: 'Veg Biryani (Full)',
+    price: '₹320',
+    tags: 'Basmati | Spices | Dum style | Hyderabadi',
+  },
+  {
+    title: 'Butter Naan (2pcs)',
+    price: '₹60',
+    tags: 'Tandoor baked | Butter | Bread',
+  },
+
+  {
+    title: 'Dal Makhani',
+    price: '₹180',
+    tags: 'Black lentils | Creamy | Punjabi dal',
+  },
+  {
+    title: 'Gulab Jamun (2 pcs)',
+    price: '₹90',
+    tags: 'Milk dumplings | Sugar syrup | Dessert',
+  },
+];
+
+const awards = [
+  {
+    imgUrl: images.award02,
+    title: 'Times Food Award',
+    subtitle: 'Best Indian Restaurant (2024)',
+  },
+  {
+    imgUrl: images.award01,
+    title: 'Best Local Eatery',
+    subtitle: 'Voted by food critics',
+  },
+  {
+    imgUrl: images.award05,
+    title: 'Top 10 Biryani',
+    subtitle: 'Regional excellence award',
+  },
+  {
+    imgUrl: images.award03,
+    title: 'Chef\'s Excellence',
+    subtitle: 'Outstanding culinary skill',
+  },
+];
+
+export default { wines, cocktails, awards };
