d0de460375b63cb78f6db4086f801411191c1191
Biswarup Pal <biswarup743428@gmail.com>
2026-01-19 17:04:14 +0530

    Fixed incorrect imports and folder structure for build

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
-import FindUs from '../container/FindUs/FindUs';
 import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './MenuItem/MenuItem';
 import Navbar from './Navbar/Navbar';
@@ -9,7 +8,6 @@ import Navbar from './Navbar/Navbar';
 export {
   SubHeading,
   Newsletter,
-  FindUs,
   FooterOverlay,
   MenuItem,
   Navbar,
