d4e9e02bd7db76b54f54491d71bec9e89fc7e645
Biswarup Pal <biswarup743428@gmail.com>
2026-01-19 16:54:33 +0530

    Fixed MenuItem and FindUs path issues

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
-import FindUs from '../container/Findus/FindUs';
+import FindUs from '../container/FindUs/FindUs';
 import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './MenuItem/MenuItem';
 import Navbar from './Navbar/Navbar';
