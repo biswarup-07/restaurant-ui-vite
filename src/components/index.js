ba74b9c130c769a7981ff3d92e4e3ec541789735
Biswarup Pal <biswarup743428@gmail.com>
2026-01-19 16:33:36 +0530

    For Hosting

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,17 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
+import FindUs from '../container/Findus/FindUs';
 import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './MenuItem/MenuItem';
 import Navbar from './Navbar/Navbar';
 
+
 export {
   SubHeading,
   Newsletter,
+  FindUs,
   FooterOverlay,
   MenuItem,
   Navbar,
+ 
 };
